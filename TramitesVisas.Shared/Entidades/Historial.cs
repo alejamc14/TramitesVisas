8b3abd284f5489fa753a7dca1e4536b888131319
paucq <calderonpaula781@gmail.com>
2024-05-30 09:58:54 -0500

    Creacion de entidad y controlador Historial

---
+++ TramitesVisas.Shared/Entidades/Historial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace TramitesVisas.Shared.Entidades
+{
+    public class Historial
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Tipo solicitud")]
+        [MaxLength(20, ErrorMessage = "No se perimten mas de 20 digitos")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string TipoSolicitud { get; set; }
+
+        [Display(Name = "Tipo solicitud")]
+        [MaxLength(20, ErrorMessage = "No se perimten mas de 20 digitos")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Estado { get; set; }
+
+
+        [JsonIgnore]
+        //public Visa Visas { get; set; }
+        //public int VisaId { get; set; }
+
+        public Renovacion Renovaciones { get; set; }
+        public int RenovacionId { get; set; }
+
+    }
+}
